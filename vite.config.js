6ff316ed9aa1514148dad2a4c0f717724dd58646
AlfZ <90620847+AlfaFza@users.noreply.github.com>
2025-04-02 12:43:02 +0530

    Update vite.config.js
    UPDATE deploy to vercel
    only have changed env.vite_base_path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-3/', // Replace with your repository name
+  base: process.env.VITE.BASE.PATH ||'/portfolio-3/', // Replace with your repository name
 })
